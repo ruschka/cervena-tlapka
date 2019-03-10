d89c75762be8c71375325444cdab3fbbc9d0a354
Vojtěch Ruschka <vojtech.ruschka@etnetera.cz>
2019-03-10 11:26:04 +0100

    Mongoose validation.

--- src/donor/DonorRegistration.js
+++ src/donor/DonorRegistration.js
@@ -3,11 +3,30 @@
 const mongoose = require('mongoose');
 
 const donorRegistrationSchema = new mongoose.Schema({
-    name: String,
-    weight: Number,
-    birthYear: Number,
-    sex: String,
-    breed: String
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    weight: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 150
+    },
+    birthYear: {
+        type: Number,
+        required: true
+    },
+    sex: {
+        type: String,
+        required: true,
+        match: /[MF]/
+    },
+    breed: {
+        type: String,
+        required: true
+    }
 });
 
 const DonorRegistration = mongoose.model('DonorRegistration', donorRegistrationSchema, 'donor-registration');
