90cd20cda17e524b6194ecce524575becd995864
Vojtěch Ruschka <vojtech.ruschka@etnetera.cz>
2019-04-14 17:36:12 +0200

    Show Recaptcha errors.

--- src/core/recaptcha/index.js
+++ src/core/recaptcha/index.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import config from "../config";
+import { success, unsuccess } from "../utils";
 
 const secret = config.recaptcha.apiKey;
 
@@ -14,25 +15,22 @@ export async function validateRecaptcha(ctx, data, actionName) {
     const recaptchaResult = result.data;
     if (!recaptchaResult.success) {
         console.error(`Recaptcha error: ${JSON.stringify(recaptchaResult)}`);
-        return {
-            success: false,
-            data: data,
-            errors: {
-                global:
-                    "Ochrana proti robotům selhala. Zkuste to prosím později znovu. Omlouváme se za problémy."
-            }
-        };
+        return unsuccess(data, {
+            global:
+                "Ochrana proti robotům selhala. Zkuste to prosím později znovu. Omlouváme se za problémy."
+        });
     }
-    if (recaptchaResult.action !== actionName || recaptchaResult.score < 0.5) {
-        return {
-            success: false,
-            data: data,
-            errors: {
-                global:
-                    "Tento formulář využívá ochranu proti robotům. Pokud toto čtete, je to omyl, za který se omlouváme. Kontaktujte nás prosím."
-            }
-        };
+    if (recaptchaResult.action !== actionName) {
+        return unsuccess(data, {
+            global: "Recaptcha actions don't match."
+        });
+    }
+    if (recaptchaResult.score < 0.5) {
+        return unsuccess(data, {
+            global:
+                "Tento formulář využívá ochranu proti robotům. Pokud toto čtete, je to omyl, za který se omlouváme. Zkuste obnovit stránku a formulář vyplnit znovu. Jestliže se chyba opakuje, kontaktujte nás prosím."
+        });
     } else {
-        return { success: true };
+        return success();
     }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-"use strict"
+"use strict";
 
 import KoaRouter from "koa-router";
-import { setTemplateData } from "../core/template";
+import { renderTemplate } from "../core/template";
 
 export const pagesRouter = new KoaRouter();
 
 pagesRouter.get("/", async (ctx, next) => {
-    setTemplateData(ctx, {});
-    await ctx.render("homepage.pug");
+    await renderTemplate(ctx, "homepage.pug");
 });
 
 pagesRouter.get("/robots.txt", async (ctx, next) => {
@@ -16,12 +15,10 @@ pagesRouter.get("/robots.txt", async (ctx, next) => {
 
 pagesRouter.get("/404", async (ctx, next) => {
     ctx.status = 404;
-    setTemplateData(ctx, {});
-    await ctx.render("error-page/404.pug");
+    await renderTemplate(ctx, "error-page/404.pug");
 });
 
 pagesRouter.get("/500", async (ctx, next) => {
     ctx.status = 500;
-    setTemplateData(ctx, {});
-    await ctx.render("error-page/500.pug");
+    await renderTemplate(ctx, "error-page/500.pug");
 });
