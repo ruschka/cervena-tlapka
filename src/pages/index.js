109fdd78506387e0fa38f2f530e5cbcbe18fee4e
Vojtěch Ruschka <vojtech.ruschka@etnetera.cz>
2019-05-06 12:30:06 +0200

    Added sitemap.xml.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,21 @@
 
 import KoaRouter from "koa-router";
 import { renderTemplate } from "../core/template";
+import sm from "sitemap";
+import config from "../core/config";
+
+const sitemap = sm.createSitemap({
+    hostname: config.server.baseUrl,
+    cacheTime: 600000,
+    urls: [
+        { url: "/", changefreq: "weekly", priority: 1 },
+        { url: "/find-donor", changefreq: "hourly", priority: 1 },
+        { url: "/register-donor", changefreq: "monthly", priority: 1 },
+        { url: "/want-to-help", changefreq: "monthly", priority: 0.5 },
+        { url: "/about-us", changefreq: "monthly", priority: 0.5 },
+        { url: "/contacts", changefreq: "monthly", priority: 0.5 }
+    ]
+});
 
 export const pagesRouter = new KoaRouter();
 
@@ -23,10 +38,24 @@ pagesRouter.get("/want-to-help", async (ctx, next) => {
 
 pagesRouter.get("/robots.txt", async (ctx, next) => {
     if (process.env.NODE_ENV === "production") {
-        ctx.response.body = "User-agent: * \n" + "Allow: /";
+        ctx.response.body =
+            "User-agent: * \n" +
+            "Allow: / \n\n" +
+            `Sitemap: ${config.server.baseUrl}/sitemap.xml`;
     } else {
         ctx.response.body = "User-agent: * \n" + "Disallow: /";
     }
+});
+
+pagesRouter.get("/sitemap.xml", async (ctx, next) => {
+    ctx.response.body = await new Promise((resolve, reject) => {
+        sitemap.toXML((err, xml) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(xml);
+        });
+    });
 });
 
 pagesRouter.get("/404", async (ctx, next) => {
